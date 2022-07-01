04a2f33fcf3c1693a9a685e2eb375dc45f94a92f
jbizima <j.bizima@alustudent.com>
2022-07-01 08:34:08 -0700

    Task 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "object-oriented programming with Python"
-str = str[0:8] + str[8:-2] + str[-2:]
+str = "object-oriented programming with Python Python is an interpreted and interactive"
+str = str[0:8] + str[8:30] + str[30:39]
 print(str) 
-#language that combines remarkable power with very clear syntax
+
