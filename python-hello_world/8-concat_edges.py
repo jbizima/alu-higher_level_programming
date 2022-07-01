2ab05f60f66f1f190d69d14aa20cce2216c6d0a0
jbizima <j.bizima@alustudent.com>
2022-07-01 08:23:28 -0700

    Task 8

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "object-oriented programming with Python"
+str = str[0:8] + str[8:-2] + str[-2:]
+print(str) 
+#language that combines remarkable power with very clear syntax
