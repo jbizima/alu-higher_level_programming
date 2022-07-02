02045386b3abbfed28b2d5699db210614080a25e
jbizima <j.bizima@alustudent.com>
2022-07-02 09:11:31 -0700

    Task 3

---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for alpha_letters in range(ord('a'), ord('z')+1):
+      if chr(alpha_letters) =="q"  or chr(alpha_letters) == "e":
+         continue
+      print("{:s}".format(chr(alpha_letters)), end="")
