c7478a31f129ec64ae099fa54ee3c1af36d591cc
jbizima <j.bizima@alustudent.com>
2022-07-29 12:34:16 -0700

    Task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Class rectangle inherent"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Class that defines a rectangle from BaseGeometry Class """
+
+    def __init__(self, width, height):
+        """ Initializes instance """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
