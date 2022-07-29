4524d76cda12e64190a409a8c417244d4bd52f6d
jbizima <j.bizima@alustudent.com>
2022-07-29 12:41:53 -0700

    Task 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-''' str() should return , based on 10-square.py '''
+"""
+more class base
+"""
 
 
 Rectangle = __import__('9-rectangle').Rectangle
+"""
+Square class
+"""
 
 
 class Square(Rectangle):
-    """ Class that defines a Square from Rectangle class """
+    """ Square Class """
+
     def __init__(self, size):
-        """ Method that initializes a Square """
-        self.integer_validator("size", size)
+        """ size init"""
         self.__size = size
         super().__init__(self.__size, self.__size)
 
-    def area(self):
-        """ Method that returns a string with the area """
-        return super().area()
-
     def __str__(self):
-        """ Special method that returns a printable string """
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
