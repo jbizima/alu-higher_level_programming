a8b4e8420327385519267d4c8cfbcb4764b9ea05
jbizima <j.bizima@alustudent.com>
2022-08-26 09:20:54 -0700

    Task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Script to create a class State
 """
+class definition of a State and an instance Base
+"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,9 +10,14 @@ Base = declarative_base()
 
 
 class State(Base):
-    """Class definition of a state that inherits of Base class
-    Args:
-        Base : class
+    """
+    State class:
+    inherits from Base
+    links to the MySQL table states
+    class attribute id that represents a column of an auto-generated,
+    unique integer, cant be null and is a primary key
+    class attribute name that represents a column of a string
+    with maximum 128 characters and cant be null
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
