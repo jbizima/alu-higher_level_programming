e42bd8cb4c306bdf01f6b89e3eb3c0d2fe856c00
jbizima <j.bizima@alustudent.com>
2022-10-03 12:37:37 -0700

    Tasks

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 let arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < 3; i++) {
+let length = arr.length;
+let i = 0;
+
+for (i = 0; i < length; i++) {
   console.log(arr[i]);
 }
