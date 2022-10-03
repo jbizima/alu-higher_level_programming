83aa357a182a481faf50f8b22a87751fb4f04118
jbizima <j.bizima@alustudent.com>
2022-10-03 11:03:16 -0700

    Tasks

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const s = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const i = 0; i < 3; i++) {
-  console.log(s[i]);
+const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const i of lang) {
+  console.log(i);
 }
