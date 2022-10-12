7664ac8dc7674f2267657cb8789f8815e18854a5
jbizima <j.bizima@alustudent.com>
2022-10-12 07:29:06 -0700

    Tasks

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/';
-const episode = process.argv[2];
 
-request(url + episode, function errorFunc (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(JSON.parse(body).title);
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/:id' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
   }
-}
+  console.log(JSON.parse(body).title);
+});
