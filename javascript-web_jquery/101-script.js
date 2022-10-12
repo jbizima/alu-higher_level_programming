0760f702f55b631846e50d440c02ca4c1b92ce84
jbizima <j.bizima@alustudent.com>
2022-10-12 11:49:21 -0700

    Tasks

---
+++ javascript-web_jquery/101-script.js
@@ -0,0 +1,12 @@
+ $(document).ready(function () {
+  $('DIV#add_item').click(() => {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+
+  $('DIV#remove_item').click(() => {
+    $('UL.my_list>li').remove();
+  });
+
+  $('DIV#clear_list').click(() => {
+    $('UL.my_list>li').remove();
+  });
---
+++ javascript-web_jquery/102-script.js
@@ -0,0 +1,14 @@
+const URL = 'https://www.fourtonfish.com/hellosalut/';
+
+function Hello (lang) {
+  $.get(`${URL}?lang=${lang}`, (data, textStatus) => {   $('DIV#hello').text(data.hello);
+    }
+  });
+}
+
+$(document).ready(function () {
+  $('#btn_translate').click(() => {
+    const lang = $('#language_code').val();
+    Hello(lang);
+  });
+});
